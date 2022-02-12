763583e966937d10762fea1aa4a57b84950720f4
Bill Dengler <codeofdusk@gmail.com>
2022-02-12 01:24:02 -0500

    Fallback to current if now is empty

--- marys/__init__.py
+++ marys/__init__.py
@@ -246,7 +246,9 @@ class Menu(MenuBase, UserDict):
             )  # Hack to work around "brunch" on some days
         elif item == "now":
             now = datetime.now(tz)
-            return self.filter(lambda x: x.start <= now <= x.end)
+            return self.filter(lambda x: x.start <= now <= x.end) or self.filter(
+                lambda x: now < x.end
+            )
         elif item == "current":
             # In older iterations of the API, "open door" was unlisted
             # so it made more sense to provide both menus on now and up next.
